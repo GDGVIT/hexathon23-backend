c95a2aa5b2877e7b7f87d5c0186a4a40f132b9c2
Dhruv Shah <dhruvshahrds@gmail.com>
2023-09-21 03:28:09 +0530

    feat: added methods for items model

--- hexathon-api/internal/models/items.go
+++ hexathon-api/internal/models/items.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/GDGVIT/hexathon23-backend/hexathon-api/internal/database"
+	"github.com/google/uuid"
+)
 
 // Item is the db model for items table
 type Item struct {
@@ -11,4 +14,38 @@ type Item struct {
 	Price       int
 	Category    Category `gorm:"foreignKey:CategoryID"`
 	CategoryID  uuid.UUID
+}
+
+// CreateItem creates a new item
+func (item *Item) CreateItem() error {
+	return database.DB.Create(item).Error
+}
+
+// UpdateItem updates an item
+func (item *Item) UpdateItem() error {
+	return database.DB.Save(item).Error
+}
+
+// DeleteItem deletes an item
+func (item *Item) DeleteItem() error {
+	return database.DB.Delete(item).Error
+}
+
+// ValidateItemName validates an item name
+func ValidateItemName(name string) bool {
+	return len(name) >= 3
+}
+
+// GetItemByID returns an item by id
+func GetItemByID(id string) (*Item, error) {
+	var item Item
+	err := database.DB.Where("id = ?", id).First(&item).Error
+	return &item, err
+}
+
+// GetItems returns a list of all items
+func GetItems() ([]Item, error) {
+	var items []Item
+	err := database.DB.Find(&items).Error
+	return items, err
 }
