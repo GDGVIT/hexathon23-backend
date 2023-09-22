ef3b85ef19df4b7de9fca16caf40a2d1540ad485
Aditya Chowdhary <adityachowdhary04@gmail.com>
2023-09-23 03:08:21 +0530

    fix: Combined submit and update

--- hexathon-api/api/v1/submissionsHandler.go
+++ hexathon-api/api/v1/submissionsHandler.go
@@ -16,7 +16,6 @@ func submissionsHandler(r fiber.Router) {
 	// Routes
 	group.Use(middleware.JWTAuthMiddleware)
 	group.Post("/submit", submitSubmission) // <server-url>/api/v1/submissions/submit
-	group.Put("/submit", updateSubmission)	// <server-url>/api/v1/submissions/submit
 
 	group.Use(middleware.IsAdminMiddleware)
 	group.Delete("/:id", deleteSubmission) // <server-url>/api/v1/submissions/:id
@@ -36,106 +35,71 @@ func submitSubmission(c *fiber.Ctx) error {
 		})
 	}
 
+	var requestBody struct {
+		FigmaURL string `json:"figmaURL"`
+		DocURL   string `json:"docURL"`
+	}
+
+	if err := c.BodyParser(&requestBody); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(schemas.InvalidBody)
+	}
+
+	if requestBody.FigmaURL == "" || requestBody.DocURL == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"detail": "Both figmaURL and docURL are required",
+		})
+	}
+
 	if team.Submitted {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"detail": "Team has already submitted",
+		submission, err := models.GetSubmissionByTeamID(team.ID.String())
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"detail": fmt.Sprintf("Error getting item: %s", err.Error()),
+			})
+		}
+
+		if submission == nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"detail": "Submission not found",
+			})
+		}
+
+		submission.FigmaURL = requestBody.FigmaURL
+		submission.DocURL = requestBody.DocURL
+
+		if err := submission.UpdateSubmission(); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"detail": fmt.Sprintf("Error updating item: %s", err.Error())})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"detail": "Update successful",
+		})
+	} else {
+		submission := &models.Submission{
+			FigmaURL:         requestBody.FigmaURL,
+			DocURL:           requestBody.DocURL,
+			Team:             *team,
+			ProblemStatement: team.ProblemStatement,
+		}
+	
+		if err := submission.CreateSubmission(); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"detail": fmt.Sprintf("Error creating submission: %s", err.Error())})
+		}
+	
+		team.Submitted = true
+		err = team.UpdateTeam()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"detail": "Internal Server Error",
+			})
+		}
+	
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"detail": "Submission successful",
 		})
 	}
-
-	var requestBody struct {
-		FigmaURL string `json:"figmaURL"`
-		DocURL   string `json:"docURL"`
-	}
-
-	if err := c.BodyParser(&requestBody); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(schemas.InvalidBody)
-	}
-
-	if requestBody.FigmaURL == "" || requestBody.DocURL == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"detail": "Both figmaURL and docURL are required",
-		})
-	}
-
-	submission := &models.Submission{
-		FigmaURL:         requestBody.FigmaURL,
-		DocURL:           requestBody.DocURL,
-		Team:             *team,
-		ProblemStatement: team.ProblemStatement,
-	}
-
-	if err := submission.CreateSubmission(); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"detail": fmt.Sprintf("Error creating submission: %s", err.Error())})
-	}
-
-	team.Submitted = true
-	err = team.UpdateTeam()
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"detail": "Internal Server Error",
-		})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"detail": "Submission successful",
-	})
-}
-
-// Update Submission based on submission ID
-func updateSubmission(c *fiber.Ctx) error {
-	team, err := models.GetTeamByName(c.Locals("team").(models.Team).Name)
-
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"detail": fmt.Sprintf("Error getting team: %s", err.Error())})
-	}
-
-	if team == nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"detail": "Team not found",
-		})
-	}
-
-	var requestBody struct {
-		FigmaURL string `json:"figmaURL"`
-		DocURL   string `json:"docURL"`
-	}
-
-	if err := c.BodyParser(&requestBody); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(schemas.InvalidBody)
-	}
-
-	if requestBody.FigmaURL == "" || requestBody.DocURL == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"detail": "Both figmaURL and docURL are required",
-		})
-	}
-
-	submission, err := models.GetSubmissionByTeamID(team.ID.String())
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"detail": fmt.Sprintf("Error getting item: %s", err.Error()),
-		})
-	}
-
-	if submission == nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"detail": "Submission not found",
-		})
-	}
-
-	submission.FigmaURL = requestBody.FigmaURL
-	submission.DocURL = requestBody.DocURL
-
-	if err := submission.UpdateSubmission(); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"detail": fmt.Sprintf("Error updating item: %s", err.Error())})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"detail": "Update successful",
-	})
 }
 
 // Deletes a submission based on submission ID
@@ -149,7 +113,7 @@ func deleteSubmission(c *fiber.Ctx) error {
 
 	if submission == nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"detail": "Submission not found", 
+			"detail": "Submission not found",
 		})
 	}
 
